1fc16043e922ec4db36af04c34382a6808cdfaeb
atombreak <trsutchochaatie2@gmail.com>
2022-06-28 13:03:35 +0200

    ankther one

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,10 +8,18 @@ const Form = ({closeForm}) => {
 	const [titleInput, setTitleInput] = useState("")
 	const [descripInput, setDescripInput] = useState("");
 	const [tasks, setTasks]	= useRecoilState(Tasks)
+	const [isDisabled, setIsDisabled] = useState(true)
 
 	const d = new Date();
-
+	
 	const addTask = () => {
+
+		if(titleInput.length < 3 && descripInput.length < 5){
+			return closeForm()
+		}else{
+			
+		}
+
 		const task = {
 			id: Math.floor((Math.random() * 10) * 3 + (Math.random() * 10) + 2 * (Math.random() * 10)),
 			title: titleInput,
@@ -29,7 +37,7 @@ const Form = ({closeForm}) => {
 		<VStack spacing="4"  maxW={{base: "100vh", sm:"80vh", md:"50vh", lg:"36vh"}} p={4} h="auto" >
 			<Input onChange={(e) => setTitleInput(e.target.value)} variant="filled" type="text" placeholder="Super Task Title" />
 			<Textarea onChange={(e) => setDescripInput(e.target.value)}  varisnt="filled"  size="md"  placeholder="Describe your SUPER task here....." />
-			<Button onClick={addTask} alignSelf="flex-end" color="aliceblue" bgColor="teal.500" mt={4}> Create It</Button>
+			<Button  onClick={addTask} alignSelf="flex-end" color="aliceblue" bgColor="teal.500" mt={4}> Create Task</Button>
 			 
 		</VStack>
 	)
